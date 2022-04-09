37409604a281153cd4252a85908a5af8c8de28b4
Gandi Purna Jen <nyonggpj9995@gmail.com>
2022-04-09 10:58:51 +0700

    modify config.json to config.js

---
+++ config/config.js
@@ -0,0 +1,26 @@
+//IMPORT PACKEGE
+require('dotenv/config')
+
+module.exports = {
+  "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DEVELOPMENT,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
+  },
+  "test": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_TEST,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
+  },
+  "production": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_PRODUCTION,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
+  }
+}
