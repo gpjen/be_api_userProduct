b0c290a1f6447c3fb3be7f381e25169ea049a6ad
Gandi Purna Jen <nyonggpj9995@gmail.com>
2022-04-09 13:43:06 +0700

    create users model and migration

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,12 @@
-//register
+// register
 exports.userRegister = async (req, res) => {
 
 }
+
+// login
+
+// get users
+
+// get user by Id
+
+// delete user by id
---
+++ migrations/20220409063118-create-users.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const {
+  sequelize
+} = require("../models");
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING(100)
+      },
+      email: {
+        type: Sequelize.STRING(50)
+      },
+      phone: {
+        type: Sequelize.STRING(20)
+      },
+      jenis_kelamin: {
+        type: Sequelize.ENUM('laki-laki', 'perempuan')
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        devaultValue: Sequelize.fn('now')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        devaultValue: Sequelize.fn('now')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
