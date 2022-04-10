ac4889a246f9f6a55d0efcb95336909a9bb9fc9d
Gandi Purna Jen <nyonggpj9995@gmail.com>
2022-04-10 23:57:30 +0700

    add validation regex and custom message errors

--- controllers/configValidation/functionCollections.js
+++ controllers/configValidation/functionCollections.js
@@ -3,28 +3,40 @@ const Validator = require('fastest-validator')
 const {
     name,
     email,
-
+    phone,
+    jenis_kelamin,
+    password
 } = require('./objValidation.js')
 
 // SUPORT VARIABLE
-const v = new Validator()
+const v = new Validator({
+    useNewCustomCheckerFunction: true,
+    messages: {
+        phoneNumber: 'format telepon tidak didukung ex: +62821-0000-0000 / 6282100000000',
+        gender: 'gender tidak didukung ex: laki-laki / perempuan',
+        passFormat: 'password harus mengandung huruf besar, huruf kecil dan angka'
+    }
+})
 
 
 validationRegister = (data) => {
-
     const schema = {
         name,
-        email
+        email,
+        phone,
+        jenis_kelamin,
+        password
     }
-
     const check = v.compile(schema)
-
     return check(data)
 
 }
 
 
-console.log(validationRegister({
+console.log('result => ', validationRegister({
     name: 'gandi jen',
-    email: 'gpjmail.com'
+    email: 'gpj@gmail.com',
+    phone: '',
+    jenis_kelamin: 'perempuan',
+    password: "backsb"
 }));
--- controllers/configValidation/objValidation.js
+++ controllers/configValidation/objValidation.js
@@ -14,5 +14,47 @@ module.exports = {
         messages: {
             email: 'email tidak valid'
         }
+    },
+    phone: {
+        type: 'string',
+        optional: true,
+        custom: (val, err) => {
+            const regExPhone = /(^\+?[0-9-]{5,15}$)/
+            if (!val || val == "") {
+                return val
+            } else if (!val.match(regExPhone)) {
+                err.push({
+                    type: 'phoneNumber'
+                })
+                return false
+            }
+        }
+    },
+    jenis_kelamin: {
+        type: 'string',
+        messages: {
+            required: 'gender wajib diisi',
+        },
+        custom: (val, err) => {
+            const regEx = /^(laki-laki|perempuan)$/
+            if (!val.match(regEx)) {
+                err.push({
+                    type: 'gender'
+                })
+                return false
+            }
+            return val
+        }
+    },
+    password: {
+        type: 'string',
+        min: 6,
+        messages: {
+            required: 'password wajib diisi',
+            stringMin: 'password mengandung minimal 6 karakter'
+        },
+        custom: (val, err) => {
+            const regExPass = / /
+        }
     }
 }
