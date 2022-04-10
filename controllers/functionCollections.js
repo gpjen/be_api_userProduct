4c41def5f620fffc67728cbb9dd13d6bc21e2a5e
Gandi Purna Jen <nyonggpj9995@gmail.com>
2022-04-10 18:38:47 +0700

    container for validation

---
+++ controllers/functionCollections.js
@@ -0,0 +1,13 @@
+exports.validationRegister = (data) => {
+    let errMsg = []
+    const {
+        name,
+        email,
+        phone,
+        jenis_kelamin,
+        password,
+        confirm_password,
+        status
+    } = data;
+
+}
