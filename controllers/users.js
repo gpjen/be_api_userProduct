b0def54e0ad1bb57f0dcd58fa227454bb24175d7
Gandi Purna Jen <nyonggpj9995@gmail.com>
2022-04-11 23:49:49 +0700

    update

--- controllers/users.js
+++ controllers/users.js
@@ -1,65 +1,63 @@
 // IMPORT PACKEGE
-const bcrypt = require('bcrypt')
-
+const bcrypt = require("bcrypt");
 
 // IMPORT MODELS
-const {
-    users
-} = require('../models')
+const { users } = require("../models");
 
 // register CREATE
 exports.userRegister = async (req, res) => {
-    try {
-        const newData = req.body
-        newData.status = newData.status || 'buyer'
+  try {
+    const newData = req.body;
+    newData.status = newData.status || "buyer";
 
-        // HASH PASSWORD
-        const salt = await bcrypt.genSalt(10)
-        newData.password = await bcrypt.hashSync(newData.password, salt)
+    // HASH PASSWORD
+    const salt = await bcrypt.genSalt(10);
+    newData.password = await bcrypt.hashSync(newData.password, salt);
 
-        // CREATE NEW USERS
-        const newUser = await users.create(newData)
-        res.status(201).json({
-            status: 'success',
-            message: `${newUser.name} registered successfully`
-        })
-    } catch (error) {
-        console.log(error.message)
-        res.status(400).json({
-            status: 'failed',
-            message: error.message
-        })
-    }
-}
+    // CREATE NEW USERS
+    const newUser = await users.create(newData);
+    res.status(201).json({
+      status: "success",
+      message: `${newUser.name} registered successfully`,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({
+      status: "failed",
+      message: error.message,
+    });
+  }
+};
 
 // login
 
 // get users
 exports.getUsers = async (req, res) => {
-    try {
-        const data = await users.findAll({
-            attributes: {
-                exclude: ['createdAt', 'updatedAt', 'isDelete']
-            }
-        })
+  try {
+    const data = await users.findAll({
+      attributes: {
+        exclude: ["createdAt", "updatedAt", "isDelete"],
+      },
+    });
 
-        if (data.length <= 0) return res.json({
-            status: 'success',
-            data: 'no data'
-        })
+    if (data.length <= 0)
+      return res.json({
+        status: "success",
+        data: "no data",
+      });
 
-        res.status(200).json({
-            status: 'success',
-            data
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({
-            status: 'failed',
-            message: error.message
-        })
-    }
-}
+    res.status(200).json({
+      status: "success",
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: "failed",
+      message: error.message,
+    });
+  }
+};
 
 // get user by Id
 
