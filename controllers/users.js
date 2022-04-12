7f497634b2950886047da026a38b08990bab0439
Gandi Purna Jen <nyonggpj9995@gmail.com>
2022-04-12 19:34:52 +0700

    add login and update error message handling

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,9 @@
 // IMPORT PACKEGE
 const bcrypt = require("bcrypt");
+const { Op } = require("sequelize");
 
 // IMPORT MODELS
-const { users, sequelize } = require("../models");
+const { users } = require("../models");
 
 // register CREATE
 exports.userRegister = async (req, res, next) => {
@@ -20,12 +21,41 @@ exports.userRegister = async (req, res, next) => {
       message: `${newUser.email} registered successfully`,
     });
   } catch (error) {
-    console.log(error.message);
+    console.log("error > ", error.message);
     next(error);
   }
 };
 
 // login
+exports.userLogin = async (req, res, next) => {
+  const { password } = req.body;
+  const email = req.body.email || "";
+  const phone = req.body.phone || "";
+  try {
+    const data = await users.findOne({
+      where: {
+        [Op.or]: [{ email }, { phone }],
+      },
+    });
+
+    if (!data) {
+      return res.status(400).json({
+        status: "failed",
+        message: "email and password dosnt match",
+      });
+    }
+
+    res.status(200).json({
+      status: data ? "success" : "failed",
+      data: data || "no data",
+      password,
+    });
+  } catch (error) {
+    error.message = `userLogin => ${error.message}`;
+    console.log("error > ", error.message);
+    next(error);
+  }
+};
 
 // get users
 exports.getUsers = async (req, res, next) => {
@@ -36,18 +66,12 @@ exports.getUsers = async (req, res, next) => {
       },
     });
 
-    if (data.length <= 0)
-      return res.json({
-        status: "success",
-        data: "no data",
-      });
-
     res.status(200).json({
       status: "success",
-      data,
+      data: data.length > 0 ? data : "no data user",
     });
   } catch (error) {
-    console.log(error);
+    console.log("error > ", error.message);
     next(error);
   }
 };
@@ -65,11 +89,28 @@ exports.getUserById = async (req, res, next) => {
     });
 
     res.status(200).json({
-      status: "success",
+      status: data ? "success" : "failed",
       data: data || `Id.${id}, has no data`,
     });
   } catch (error) {
-    console.log(error.message);
+    console.log("error > ", error.message);
+    next(error);
+  }
+};
+
+// update data user
+exports.updateUser = async (req, res, next) => {
+  const { id } = req.params;
+  const newData = req.body;
+  try {
+    const data = await users.update(newData, { where: { id } });
+
+    res.status(200).json({
+      status: data > 0 ? "success" : "failed",
+      message: `${data} data updated`,
+    });
+  } catch (error) {
+    console.log("error > ", error.message);
     next(error);
   }
 };
@@ -84,7 +125,7 @@ exports.delUserById = async (req, res, next) => {
       message: `${data} data deleted`,
     });
   } catch (error) {
-    console.log("==> ", error.message);
+    console.log("error > ", error.message);
     next(error);
   }
 };
