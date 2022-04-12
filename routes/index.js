ff78e0704e691d9d8d966f65e664d333fcc9d4bb
Gandi Purna Jen <nyonggpj9995@gmail.com>
2022-04-12 17:11:20 +0700

    add midleware and controller get user by Id and delete user by Id

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 //VALIDATION
-const { registerValidation } = require("../midleware/validator/funcValidator");
+const {
+  registerValidation,
+  validationOneParamsId,
+} = require("../midleware/validator/usersValidator");
 
 //USER
-const { getUsers, getUserById, userRegister } = require("../controllers/users");
+const {
+  getUsers,
+  getUserById,
+  userRegister,
+  delUserById,
+} = require("../controllers/users");
 
 router.get("/users", getUsers);
-router.get("/user/:id", getUserById);
+router.get("/user/:id", validationOneParamsId, getUserById);
 router.post("/register", registerValidation, userRegister);
+router.delete("/user/:id", validationOneParamsId, delUserById);
 
 module.exports = router;
