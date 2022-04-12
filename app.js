a3d3bb12a9bc2385c352600676b04ec0cc0da222
Gandi Purna Jen <nyonggpj9995@gmail.com>
2022-04-12 15:04:16 +0700

    update errors handling

--- app.js
+++ app.js
@@ -1,53 +1,49 @@
 // IMPORT MODULE
-require('dotenv/config')
-const express = require('express')
-const morgan = require('morgan')
-const {
-    sequelize
-} = require('./models')
+require("dotenv/config");
+const express = require("express");
+const morgan = require("morgan");
+const { sequelize } = require("./models");
 
 // IMPORT SCRIPT
-const routes = require('./routes')
+const routes = require("./routes");
 
 // VARIABELS
-const app = express()
-const port = process.env.APP_PORT || 3000
+const app = express();
+const port = process.env.APP_PORT || 3000;
 
 // MIDLEWARE
-app.use(express.json())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 // ROUTES
-app.use('/api/v1', routes)
-
+app.use("/api/v1", routes);
 
 // ROUTES HANDLING
 app.use((req, res, next) => {
-    const err = new Error(`${req.method} => ${req.headers.host}${req.path} not found`)
-    err.status = 404
-    next(err)
-})
+  const err = new Error(
+    `${req.method} => ${req.headers.host}${req.path} not found`
+  );
+  err.status = 404;
+  next(err);
+});
 
 // ERRORS HANDLING
 app.use((err, req, res, next) => {
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
-    res.status(err.status || 500).json({
-        status: 'failed',
-        message: err.message
-    })
-
-})
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500).json({
+    status: "failed",
+    message: err.message,
+  });
+});
 
 app.listen(port, async () => {
-    try {
-        await sequelize.authenticate()
-        console.log(`success connecting to database`);
-    } catch (error) {
-        console.log({
-            status: 'failed to connect database',
-            message: error.message
-        });
-    }
-    console.log(`server running on port ${port}`);
-})
+  try {
+    await sequelize.authenticate();
+    console.log("==>", `success connecting to database`);
+  } catch (error) {
+    console.log("==> ", error.message);
+  }
+  console.log("==>", `server running on port ${port}`);
+});
