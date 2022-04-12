2ebc5d8c557732eef209448e1ac01e7bbcc2b7b5
Gandi Purna Jen <nyonggpj9995@gmail.com>
2022-04-12 15:04:57 +0700

    add midleware

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,14 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 //VALIDATION
-const {
-    registerValidation
-} = require('../midleware/validator/funcValidator')
+const { registerValidation } = require("../midleware/validator/funcValidator");
 
 //USER
-const {
-    getUsers,
-    userRegister
-} = require('../controllers/users')
+const { getUsers, getUserById, userRegister } = require("../controllers/users");
 
-router.get('/user', getUsers)
-router.post('/register', registerValidation, userRegister)
+router.get("/users", getUsers);
+router.get("/user/:id", getUserById);
+router.post("/register", registerValidation, userRegister);
 
-module.exports = router
+module.exports = router;
