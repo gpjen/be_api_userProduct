f465d47963d45f5e09f200ed3cedea601c6443d3
Gandi Purna Jen <nyonggpj9995@gmail.com>
2022-04-12 19:35:08 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -13,11 +13,15 @@ const {
   getUserById,
   userRegister,
   delUserById,
+  updateUser,
+  userLogin,
 } = require("../controllers/users");
 
 router.get("/users", getUsers);
 router.get("/user/:id", validationOneParamsId, getUserById);
+router.patch("/user/:id", updateUser);
 router.post("/register", registerValidation, userRegister);
+router.post("/login", userLogin);
 router.delete("/user/:id", validationOneParamsId, delUserById);
 
 module.exports = router;
